bfe0ada30939f473a950149da5deeb411ac8e256
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-05-31 00:35:17 +0200

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -2,16 +2,9 @@
 
   require('../model/loginModel.php');
 
-  if ( isset($_POST['loginEmail']) && isset($_POST['loginPassword']) ) {
-    $pswd = testLogin($_POST['loginEmail']);
-
-    if ($pswd == $_POST['loginPassword']) {
+  $pswd = testLogin($_POST['loginEmail']);
+  if ( isset($_POST['loginEmail']) && isset($_POST['loginPassword']) && $pswd == $_POST['loginPassword'] ) {
       require('../index.php');
-    }
-    else {
-      require('../view/loginView.php');
-    }
-
   }
 
   else {
--- view/loginView.php
+++ view/loginView.php
@@ -8,14 +8,14 @@
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="template/assets/css/normalize.css">
-    <link rel="stylesheet" href="template/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="template/assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="template/assets/css/themify-icons.css">
-    <link rel="stylesheet" href="template/assets/css/flag-icon.min.css">
-    <link rel="stylesheet" href="template/assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="../template/assets/css/normalize.css">
+    <link rel="stylesheet" href="../template/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../template/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../template/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="../template/assets/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../template/assets/css/cs-skin-elastic.css">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
-    <link rel="stylesheet" href="template/assets/scss/style.css">
+    <link rel="stylesheet" href="../template/assets/scss/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
