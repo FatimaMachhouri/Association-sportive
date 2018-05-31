868538eeeef0a29a6f94553a9ca2c7da5db9399f
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-05-31 02:11:05 +0200

    update login

--- controller/login.php
+++ controller/login.php
@@ -4,6 +4,8 @@
   require('../model/loginModel.php');
 
 
+    require('../view/loginView.php');
+
     while ( !(isset($_POST['loginEmail'])) && !(isset($_POST['loginPassword'])) ) {
       require('../view/loginView.php');
     }
