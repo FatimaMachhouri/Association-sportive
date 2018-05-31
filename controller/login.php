4c41ef351bebaf071e7e979f7c37498240d7928e
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-05-31 02:09:34 +0200

    update loginView

--- controller/login.php
+++ controller/login.php
@@ -4,10 +4,9 @@
   require('../model/loginModel.php');
 
 
-    if ( isset($_POST['loginEmail']) && isset($_POST['loginPassword']) ) {
+    while ( !(isset($_POST['loginEmail'])) && !(isset($_POST['loginPassword'])) ) {
       require('../view/loginView.php');
     }
 
-    else {
-      require('../index.php');
-    }
+
+    require('../index.php')
