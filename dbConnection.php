8163947ff2a27e7501792eae16b60622d059c683
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-06-02 19:02:04 +0200

    Update connect

--- dbConnection.php
+++ dbConnection.php
@@ -4,8 +4,7 @@
 function dbConnection() {
   try
   {
-    //$db = new PDO('pgsql:host=ec2-79-125-117-53.eu-west-1.compute.amazonaws.com;dbname=dambs0svsuad70;', 'crpebwcmpuoamo', '103c03bc05034cf25326ff0b9fbf5fb01834de6605e190d507ce9193aadae49f');
-    $db = new PDO('pgsql:host=localhost;dbname=BDDAssociationSportive;', 'postgres', 'amalikmchri2010');
+    $db = new PDO('pgsql:host=ec2-79-125-117-53.eu-west-1.compute.amazonaws.com;dbname=dambs0svsuad70;', 'crpebwcmpuoamo', '103c03bc05034cf25326ff0b9fbf5fb01834de6605e190d507ce9193aadae49f');
   }
   catch(Exception $e)
   {
