1b01751b1b5e83b66c829770ee529934a7deb42f
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-06-03 18:12:23 +0200

    Vars envs, test heroku

--- dbConnection.php
+++ dbConnection.php
@@ -7,10 +7,10 @@ function dbConnection() {
     //$db = new PDO('pgsql:host=ec2-79-125-117-53.eu-west-1.compute.amazonaws.com;dbname=dambs0svsuad70;', 'crpebwcmpuoamo', '103c03bc05034cf25326ff0b9fbf5fb01834de6605e190d507ce9193aadae49f');
     $host = getenv("Host");
     $database = getenv("Database");
-    $user = getenv("User");
+    $username = getenv("User");
     $password = getenv("Password");
 
-    $db = new PDO('pgsql:host=$host; dbname=$database; $user, $password');
+    $db = new PDO("pgsql:host=$host; dbname=$database", $username, $password);
 
 
 
