4300e8c5ddd07598e732376b73abf7b8197a3f5e
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-06-03 17:51:33 +0200

    Env Heroku

--- dbConnection.php
+++ dbConnection.php
@@ -5,7 +5,7 @@ function dbConnection() {
   try
   {
     //$db = new PDO('pgsql:host=ec2-79-125-117-53.eu-west-1.compute.amazonaws.com;dbname=dambs0svsuad70;', 'crpebwcmpuoamo', '103c03bc05034cf25326ff0b9fbf5fb01834de6605e190d507ce9193aadae49f');
-    $db = new PDO('pgsql:host=getenv(\'Host\');dbname=getenv("Database");', 'getenv("User")', 'getenv("Password")');
+    $db = new PDO('pgsql:host=getenv(Host);dbname=getenv("Database");', 'getenv("User")', 'getenv("Password")');
 
   }
   catch(Exception $e)
