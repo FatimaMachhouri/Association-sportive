8d0d67d321bfd3351e5c96606571f1b708900d2a
fatimamachhouri <fatimamachhouri@MacBook-Pro-de-Fatima.local>
2018-06-03 17:35:14 +0200

    $_ENV

--- dbConnection.php
+++ dbConnection.php
@@ -5,7 +5,7 @@ function dbConnection() {
   try
   {
     //$db = new PDO('pgsql:host=ec2-79-125-117-53.eu-west-1.compute.amazonaws.com;dbname=dambs0svsuad70;', 'crpebwcmpuoamo', '103c03bc05034cf25326ff0b9fbf5fb01834de6605e190d507ce9193aadae49f');
-    $db = new PDO('pgsql:host=.$_ENV["Host"].;dbname=.$_ENV["Database"].;', '.$_ENV["User"].', '.$_ENV["Passwor"].');
+    $db = new PDO('pgsql:host=$_ENV["Host"];dbname=$_ENV["Database"];', '$_ENV["User"]', '$_ENV["Password"]');
 
   }
   catch(Exception $e)
